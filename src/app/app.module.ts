cf19d156f5f34c3a150b2fa9242406b93ab68815
Jim Creasman <jdcreasman@gmail.com>
2017-01-19 18:02:01 -0500

    Initial code

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+/* Page imports */
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { TabsPage } from '../pages/tabs/tabs';
+import { RegisterPage } from '../pages/register/register';
+/* Service imports */
+import { AuthService } from '../providers/auth-service';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    TabsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    TabsPage
+  ],
+  providers: [AuthService,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
