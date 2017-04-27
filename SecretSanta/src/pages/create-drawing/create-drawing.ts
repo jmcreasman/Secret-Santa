47019b4f3ae97c769d0e598ad8266972552abd1e
Jon <jmcreasman89@gmail.com>
2017-04-27 17:50:06 -0400

    saving progress on create drawing form builder

--- SecretSanta/src/pages/create-drawing/create-drawing.ts
+++ SecretSanta/src/pages/create-drawing/create-drawing.ts
@@ -7,10 +7,46 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CreateDrawingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  fields: Array<{title: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.fields = [
+      { title: 'Test' },
+    ]
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreateDrawingPage');
+  }
+
+  field = null;
+
+  fieldOptionsVisible = false;
+  showFieldOptions() {
+    console.log('click');
+    this.fieldOptionsVisible = true;
+  }
+  hideFieldOptions() {
+    console.log('click');
+    this.fieldOptionsVisible = false;
+  }
+
+  //Form field functions
+  createParagraphField() {
+    console.log('click');
+    this.fieldOptionsVisible = false;
+  }
+  createInputField() {
+    console.log('click');
+    this.fieldOptionsVisible = false;
+  }
+  createMultipleChoiceField() {
+    console.log('click');
+    this.fieldOptionsVisible = false;
+  }
+  createCheckboxField() {
+    console.log('click');
+    this.fieldOptionsVisible = false;
   }
 
 }
